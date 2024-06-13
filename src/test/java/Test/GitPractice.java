61163ef3ab00b2e83325ff98d83d72357b2e9072
dipendrakokate96 <dipendrakokate6@gmail.com>
2024-06-13 17:25:17 +0530

    my change in code one

--- src/test/java/Test/GitPractice.java
+++ src/test/java/Test/GitPractice.java
@@ -3,6 +3,7 @@ package Test;
 public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("git practice to push eveyday work...");
+		System.out.println("git change in to the code");
 	}
 
 }
